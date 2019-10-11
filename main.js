dc2303fa347b2125cefa0d1e7bd4d478f4e5d5b1
Natawon <natawon9@gmail.com>
2019-10-11 19:14:02 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+//start mysql connection
+var connection = mysql.createConnection({
+  host     : 'monitor.open-cdn.com', //mysql database host name
+  user     : 'root', //mysql database user name
+  password : 'dootvazws3e', //mysql database password
+  database : 'dummy_db' //mysql database name
+});
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
+//end mysql connection
+
+//start body-parser configuration
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+//end body-parser configuration
+
+//create app server
+var server = app.listen(9000,  "0.0.0.0", function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
+//rest api to get all results
+app.get('/customer/v1', function (req, res) {
+   connection.query('select * from customer', function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+//rest api to get a single employee data
+app.get('/employees/v1/:id', function (req, res) {
+   console.log(req);
+   connection.query('select * from employee where id=?', [req.params.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+//rest api to create a new record into mysql database
+app.post('/employees/v1', function (req, res) {
+   var postData  = req.body;
+   connection.query('INSERT INTO employee SET ?', postData, function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+//rest api to update record into mysql database
+app.put('/employees/v1', function (req, res) {
+   connection.query('UPDATE `employee` SET `employee_name`=?,`employee_salary`=?,`employee_age`=? where `id`=?', [req.body.employee_name,req.body.employee_salary, req.body.employee_age, req.body.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+//rest api to delete record from mysql database
+app.delete('/employees/v1', function (req, res) {
+   console.log(req.body);
+   connection.query('DELETE FROM `employee` WHERE `id`=?', [req.body.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end('Record has been deleted!');
+	});
+});
